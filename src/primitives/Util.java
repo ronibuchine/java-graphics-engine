48d5413f6095fef943bbbb9fd5db6ef03bb14f4c
eliez <eliezery@g.jct.ac.il>
2021-03-02 21:38:19 +0200

    Added code for Util class

--- src/primitives/Util.java
+++ src/primitives/Util.java
@@ -3,16 +3,21 @@ package primitives;
  * Class Util is a utility class used to help avoid floating point and double errors in calculations
  * @author Roni Buchine and Eliezer Jacobs
  */
+
+//all copied from slides
 public class Util { //is this class meant to be an interface?
 
-    public boolean isZero(double n) {
-        // Eliezer how do we implement this method?
-        return n == 0;
+    private static int getExp(double n) {
+        return (int)((Double.doubleToRawLongBits(n) >> 52) & 0x7FFL) - 1023;
     }
 
-    public double alignZero(double n) {
-        // also how do we implement this one?
-        return 0;
+    public static boolean isZero(double n) {
+        // Eliezer how do we implement this method?
+        return getExp(n) < -20;
+    }
+
+    public static double alignZero(double n) {
+        return getExp(n) < -20 ? 0.0 : n;
     }
 
 }
