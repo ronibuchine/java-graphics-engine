db7960435510abe0602f918a549291b74a3a8d75
Roni Buchine <ronibuchine@gmail.com>
2021-03-06 19:33:51 +0200

    polygon already implements geometry

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -5,7 +5,7 @@ import primitives.Point3D;
 /**
  * @author Roni Buchine and Eliezer Jacobs
  */
-public class Triangle extends Polygon implements Geometry {
+public class Triangle extends Polygon {
 
     public Triangle(Point3D p0, Point3D p1, Point3D p2) {
         super(p0, p1, p2); //apparently this calls the SuperClass's constructor
