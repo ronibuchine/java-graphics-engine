5baac91c28ff41930e2fe5af58211d7047dbc5f3
unexciting_fire_dragon <41909042+alostninja@users.noreply.github.com>
2021-04-18 22:16:15 +0300

    Update CylinderTests.java

--- src/unittests/geometries/CylinderTests.java
+++ src/unittests/geometries/CylinderTests.java
@@ -1,7 +1,6 @@
 package unittests.geometries;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
