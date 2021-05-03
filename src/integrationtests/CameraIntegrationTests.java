4d39f63f3bed600d4d2b3e2a37e36dd477f90cf1
unexciting_fire_dragon <41909042+alostninja@users.noreply.github.com>
2021-05-03 18:59:52 +0300

    Update CameraIntegrationTests.java

--- src/integrationtests/CameraIntegrationTests.java
+++ src/integrationtests/CameraIntegrationTests.java
@@ -9,37 +9,72 @@ import org.junit.Test;
 
 import elements.Camera;
 import geometries.Sphere;
-import primitives.Point3D;
-import primitives.Ray;
-import primitives.Vector;
+import geometries.Geometries;
+import primitives.*;
 
 public class CameraIntegrationTests {
 
-    static final double ACCURACY = 0.0000000001;
+    Camera cam = new Camera(new Point3D(1.5, -1, 1.5), new Vector(0, 1, 0), new Vector(0, 0, 1)).setViewPlaneSize(3, 3).setDistance(1);
 
     @Test
     public void testConstructRayThroughPixelSphere() {
 
         // TC01: sphere of r=1 centered before a 3x3 viewplane
-        Camera cam = new Camera(new Point3D(1.5, -1, 1.5), new Vector(0, 1, 0), new Vector(0, 0, 1));
-        cam.setViewPlaneSize(3, 3);
-        cam.setDistance(1);
 
         Sphere s1 = new Sphere(new Point3D(1.5, 2, 1.5), 1);
 
-        LinkedList<Ray> raylist = new LinkedList<>();
-        for (int j = 0; j < 3; j++) {
+        LinkedList<Point3D> list = new LinkedList<>();
+        for (int j = 0; j < 3; ++j) {
             for (int i = 0; i < 3; i++) {
-                raylist.add(cam.constructRayThroughPixel(3, 3, j, i));
+                list.addAll(new Geometries(s1).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
             }
         }
+        assertEquals("One Camera ray intersects sphere", 2, list.size());
 
-        LinkedList<Point3D> returnlist = new LinkedList<>();
-        for (Ray ray : raylist) {
-            returnlist.addAll(s1.findIntersections(ray));
+
+        //TC02: sphere of r=2.5, overlapping view plane
+        Sphere s2 = new Sphere(new Point3D(1.5, 2, 1.5), 2.5);
+
+        list.clear();
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; i++) {
+                list.addAll(new Geometries(s2).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
+            }
         }
-        assertEquals(2, returnlist.size(), ACCURACY);
+        assertEquals("All of Camera's rays intersect sphere", 18, list.size());
 
+        //TC03: sphere of r=2, overlapping view plane except for corners
+        Sphere s3 = new Sphere(new Point3D(1.5, 1.5, 1.5), 2);
+
+        list.clear();
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; i++) {
+                list.addAll(new Geometries(s3).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
+            }
+        }
+        assertEquals("Most of Camera's rays intersect sphere", 10, list.size());
+
+        //TC04: sphere overlaps Camera, resulting in one intersection per ray
+        Sphere s4 = new Sphere(new Point3D(1.5, 0, 1.5), 4);
+
+        list.clear();
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; i++) {
+                list.addAll(new Geometries(s4).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
+            }
+        }
+        assertEquals("All of Camera's rays intersect sphere once", 9, list.size());
+
+        //TC05: sphere behing Camera; no intersections
+        Sphere s5 = new Sphere(new Point3D(1.5, -2, 1.5), .5);
+
+        list.clear();
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; i++) {
+                list.addAll(new Geometries(s5).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
+            }
+        }
+        assertEquals("None of Camera's rays intersect sphere", 0, list.size());
     }
 
     @Test
