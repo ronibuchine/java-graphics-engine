330fedbe9fb16c078492abbb6b9637bc80fa8d72
unexciting_fire_dragon <41909042+alostninja@users.noreply.github.com>
2021-05-03 20:01:58 +0300

    added tests from slides

--- src/integrationtests/CameraIntegrationTests.java
+++ src/integrationtests/CameraIntegrationTests.java
@@ -1,7 +1,6 @@
 package integrationtests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import java.util.LinkedList;
 
@@ -9,12 +8,15 @@ import org.junit.Test;
 
 import elements.Camera;
 import geometries.Sphere;
+import geometries.Triangle;
 import geometries.Geometries;
+import geometries.Plane;
 import primitives.*;
 
 public class CameraIntegrationTests {
 
     Camera cam = new Camera(new Point3D(1.5, -1, 1.5), new Vector(0, 1, 0), new Vector(0, 0, 1)).setViewPlaneSize(3, 3).setDistance(1);
+    LinkedList<Point3D> list = new LinkedList<>();
 
     @Test
     public void testConstructRayThroughPixelSphere() {
@@ -23,7 +25,6 @@ public class CameraIntegrationTests {
 
         Sphere s1 = new Sphere(new Point3D(1.5, 2, 1.5), 1);
 
-        LinkedList<Point3D> list = new LinkedList<>();
         for (int j = 0; j < 3; ++j) {
             for (int i = 0; i < 3; i++) {
                 list.addAll(new Geometries(s1).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
@@ -79,12 +80,61 @@ public class CameraIntegrationTests {
 
     @Test
     public void testConstructRayThroughPixelPlane() {
-        fail("unimplemented");
+
+        //TC01: plane is parallel to view plane
+        Plane p1 = new Plane(new Point3D(0, 3, 0), new Point3D(-3, 3, 5), new Point3D(5, 3, -1));
+
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; i++) {
+                list.addAll(new Geometries(p1).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
+            }
+        }
+        assertEquals("All of Camera's rays hit plane", 9, list.size());
+
+        //TC02: plane slopes slightly away from view plane
+        Plane p2 = new Plane(new Point3D(0, 2, 0), new Point3D(3, 2, 0), new Point3D(1.5, 0, 4));
+
+        list.clear();
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; i++) {
+                list.addAll(new Geometries(p2).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
+            }
+        }
+        assertEquals("All of Camera's rays hit plane", 9, list.size());
+        
+        //TC02: plane slopes strongly away from view plane
+        Plane p3 = new Plane(new Point3D(0, 5, 0), new Point3D(3, 5, 0), new Point3D(1.5, -1, 4));
+
+        list.clear();
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; i++) {
+                list.addAll(new Geometries(p3).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
+            }
+        }
+        assertEquals("Some of Camera's rays hit plane", 6, list.size());
     }
 
     @Test
     public void testConstructRayThroughPixelTriangle() {
-        fail("unimplemented");
+        
+        //TC01: middle ray hits Triangle
+        Triangle t1 = new Triangle(new Point3D(1, 0, 1), new Point3D(2, 0, 1), new Point3D(1.5, 0, 2));
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; i++) {
+                list.addAll(new Geometries(t1).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
+            }
+        }
+        assertEquals("Camera's middle ray hits triangle", 1, list.size());
+
+        //TC02: middle and top-center ray hit Triangle
+        list.clear();
+        Triangle t2 = new Triangle(new Point3D(1, 0, 1), new Point3D(2, 0, 1), new Point3D(1.5, 0, 3));
+        for (int j = 0; j < 3; ++j) {
+            for (int i = 0; i < 3; i++) {
+                list.addAll(new Geometries(t2).findIntersections(cam.constructRayThroughPixel(3, 3, j, i)));
+            }
+        }
+        assertEquals("Some of Camera's rays hit plane", 2, list.size());
     }
 
 }
