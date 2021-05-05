0422f9bded15d50f1c49f515ed01dd5021cb1ae6
unexciting_fire_dragon <41909042+alostninja@users.noreply.github.com>
2021-05-05 10:43:01 +0300

    added RayTests class

---
+++ src/unittests/RayTests.java
@@ -0,0 +1,18 @@
+package unittests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Unit testing class for {@link Ray} methods.
+ * 
+ * @author Roni Buchine
+ * @author Eliezer Jacobs
+ */
+public class RayTests {
+    @Test
+    public void testFindClosestPoint() {
+        fail("unimplemented");
+    }
+}
