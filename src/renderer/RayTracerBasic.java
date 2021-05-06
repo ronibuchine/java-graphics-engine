42979623e4a555c70522c982e706859d5f3cb4ce
ronibuchine <ronibuchine@gmail.com>
2021-05-06 13:58:12 +0300

    added Render and ray tracer classes

---
+++ src/renderer/RayTracerBasic.java
@@ -0,0 +1,19 @@
+package renderer;
+
+import primitives.Color;
+import primitives.Ray;
+import scene.Scene;
+
+public class RayTracerBasic extends RayTraceBase {
+
+    protected RayTracerBasic(Scene scene) {
+        super(scene);
+    }
+
+    @Override
+    public Color traceRay(Ray r) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
