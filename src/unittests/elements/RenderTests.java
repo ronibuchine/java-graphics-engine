db0f09c487dd81c3f09d1056b90513e658d79014
ronibuchine <ronibuchine@gmail.com>
2021-05-06 14:09:11 +0300

    fixed issues with render tests

--- src/unittests/elements/RenderTests.java
+++ src/unittests/elements/RenderTests.java
@@ -11,7 +11,8 @@ import scene.Scene;
 /**
  * Test rendering a basic image
  * 
- * @author Dan
+ * @author Roni Buchine
+ * @author Eliezer Jacobs
  */
 public class RenderTests {
 	private Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
@@ -30,23 +31,26 @@ public class RenderTests {
 				.setBackground(new Color(75, 127, 90));
 
 		scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50),
-				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)), // up left
-				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up right
-				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)), // down left
-				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100))); // down right
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)), // up
+																													// left
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up
+																													// right
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)), // down
+																														// left
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100))); // down
+																													// right
 
 		ImageWriter imageWriter = new ImageWriter("base render test", 1000, 1000);
-		Render render = new Render() //
-				.setImageWriter(imageWriter) //
+		Render render = new Render().setImageWriter(imageWriter) //
 				.setScene(scene) //
 				.setCamera(camera) //
-				.setRayTracer(new BasicRayTracer(scene));
+				.setRayTracer(new RayTracerBasic(scene));
 
 		render.renderImage();
 		render.printGrid(100, new Color(java.awt.Color.YELLOW));
 		render.writeToImage();
 	}
-	
+
 	/**
 	 * Test for XML based scene - for bonus
 	 */
@@ -55,18 +59,17 @@ public class RenderTests {
 		Scene scene = new Scene("XML Test scene");
 		// enter XML file name and parse from XML file into scene object
 		// ...
-		
+
 		ImageWriter imageWriter = new ImageWriter("xml render test", 1000, 1000);
 		Render render = new Render() //
 				.setImageWriter(imageWriter) //
 				.setScene(scene) //
 				.setCamera(camera) //
-				.setRayTracer(new BasicRayTracer(scene));
+				.setRayTracer(new RayTracerBasic(scene));
 
 		render.renderImage();
 		render.printGrid(100, new Color(java.awt.Color.YELLOW));
 		render.writeToImage();
 	}
 
-	
 }
