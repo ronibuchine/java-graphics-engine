b9504e11829c78ef3266630bbf8bd78d7c6d1df2
unexciting_fire_dragon <41909042+alostninja@users.noreply.github.com>
2021-05-13 09:24:49 +0300

    Added some Moodle

--- src/unittests/elements/RenderTests.java
+++ src/unittests/elements/RenderTests.java
@@ -1,6 +1,4 @@
-package unittests.renderer;
-
-import java.io.File;
+package unittests.elements;
 
 import org.junit.Test;
 
@@ -9,7 +7,6 @@ import geometries.*;
 import primitives.*;
 import renderer.*;
 import scene.Scene;
-import scene.SceneBuilder;
 
 /**
  * Test rendering a basic image
@@ -22,12 +19,11 @@ public class RenderTests {
 			.setViewPlaneSize(500, 500);
 
 	/**
-	 * Produce a scene with basic 3D model and render it into a jpeg image with a
+	 * Produce a scene with basic 3D model and render it into a png image with a
 	 * grid
 	 */
 	@Test
 	public void basicRenderTwoColorTest() {
-
 		Scene scene = new Scene("Test scene")//
 				.setAmbientLight(new AmbientLight(new Color(255, 191, 191), 1)) //
 				.setBackground(new Color(75, 127, 90));
@@ -42,11 +38,9 @@ public class RenderTests {
 				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100))); // down
 																													// right
 
-		// TC01: first rendering test (non XML)
-		ImageWriter imageWriter = new ImageWriter("TC01", 1000, 1000);
+		ImageWriter imageWriter = new ImageWriter("base render test", 1000, 1000);
 		Render render = new Render() //
 				.setImageWriter(imageWriter) //
-				.setScene(scene) //
 				.setCamera(camera) //
 				.setRayTracer(new BasicRayTracer(scene));
 
@@ -60,17 +54,13 @@ public class RenderTests {
 	 */
 	@Test
 	public void basicRenderXml() {
-
+		Scene scene = new Scene("XML Test scene");
 		// enter XML file name and parse from XML file into scene object
-		SceneBuilder sceneBuilder = new SceneBuilder("XML Test scene",
-				new File("src/unittests/xml/basicRenderTestTwoColors.xml").getPath());
-		Scene scene = sceneBuilder.build();
+		// ...
 
-		// XML_TC01: parse from XML file
-		ImageWriter imageWriter = new ImageWriter("XML_TC01", 1000, 1000);
+		ImageWriter imageWriter = new ImageWriter("xml render test", 1000, 1000);
 		Render render = new Render() //
 				.setImageWriter(imageWriter) //
-				.setScene(scene) //
 				.setCamera(camera) //
 				.setRayTracer(new BasicRayTracer(scene));
 
@@ -79,36 +69,34 @@ public class RenderTests {
 		render.writeToImage();
 	}
 
+	// For stage 6 - please disregard in stage 5
+	/**
+	 * Produce a scene with basic 3D model - including individual lights of the bodies 
+	 * and render it into a png image with a grid
+	 */
 	@Test
-	public void testTubeRender() {
-		SceneBuilder builder = new SceneBuilder("XML Tube test scene",
-				new File("src/unittests/xml/tubes.xml").getPath());
-		Scene scene = builder.build();
+	public void basicRenderMultiColorTest() {
+		Scene scene = new Scene("Test scene")//
+				.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2)); //
 
-		// TC02: xml test with tubes and cylinders
-		Camera cam = new Camera(new Point3D(0, 25, 50), new Vector(0, 0, -1), new Vector(0, 1, 0)).setViewPlaneSize(1.08, .72);
-		ImageWriter writer = new ImageWriter("XML_TC02", 1080, 720);
+		scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50) //
+				.setEmission(new Color(java.awt.Color.CYAN)), //
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)) // up left
+						.setEmission(new Color(java.awt.Color.GREEN)),
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up right
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)) // down left
+						.setEmission(new Color(java.awt.Color.RED)),
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100)) // down right
+						.setEmission(new Color(java.awt.Color.BLUE)));
 
-		Render render = new Render()
-				.setImageWriter(writer)
-				.setRayTracer(new BasicRayTracer(scene))
-				.setCamera(cam)
-				.setScene(scene);
+		ImageWriter imageWriter = new ImageWriter("color render test", 1000, 1000);
+		Render render = new Render() //
+				.setImageWriter(imageWriter) //
+				.setCamera(camera) //
+				.setRayTracer(new BasicRayTracer(scene));
 
 		render.renderImage();
-		render.writeToImage();
-
-		// TC03: xml test with rotation and movement
-		writer.setImageName("XML_TC03");
-		cam.yaw(40).move(20, 0, 0);
-		render.renderImage();
-		render.writeToImage();
-
-		// TC04: for fun
-		writer.setImageName("XML_TC04");
-		cam.move(10, 15, 10).pitch(-20);
-		render.renderImage();
+		render.printGrid(100, new Color(java.awt.Color.WHITE));
 		render.writeToImage();
 	}
-
 }
--- src/unittests/geometries/GeometriesTests.java
+++ src/unittests/geometries/GeometriesTests.java
@@ -3,7 +3,6 @@ package unittests.geometries;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.Test;
