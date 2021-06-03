fdcf1c13a176254d3fb7337c07956685f6d06605
ronibuchine <ronibuchine@gmail.com>
2021-06-03 19:15:09 +0300

    made tests a little better

--- src/unittests/lights/ReflectionRefractionTests.java
+++ src/unittests/lights/ReflectionRefractionTests.java
@@ -174,7 +174,7 @@ public class ReflectionRefractionTests {
 	public void testDiffusion() {
 		Camera camera = new Camera(new Point3D(0, 0, 300), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
 				.setViewPlaneSize(150, 150).setDistance(300);
-		scene.geometries.add(new Sphere(new Point3D(25, 0, -50), 15) //
+		scene.geometries.add(new Sphere(new Point3D(25, 0, -100), 15) //
 				.setEmission(new Color(140, 25, 25)) //
 				.setMaterial(new Material().setKd(0.5).setKs(0.7).setShininess(200).setKt(0.05)),
 				new Sphere(new Point3D(-25, 0, -50), 15) //
@@ -202,12 +202,12 @@ public class ReflectionRefractionTests {
 		Camera camera = new Camera(new Point3D(0, 0, 300), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
 				.setViewPlaneSize(150, 150).setDistance(300);
 		scene.geometries.add(
-				new Plane(new Vector(0, 1, 0), new Point3D(0, -50, 0)).setEmission(new Color(200, 0, 0))
-						.setMaterial(new Material().setKd(.5)),
-				new Tube(10, new Ray(Point3D.ZERO, new Vector(1, 10, 1))).setEmission(new Color(0, 0, 255))
-						.setMaterial(new Material().setKd(.5).setKr(.8).setGloss(3)));
+				new Plane(new Vector(0, 1, 0), new Point3D(0, -50, 0)).setEmission(new Color(100, 50, 70))
+						.setMaterial(new Material().setKd(.5).setKs(.6).setShininess(100)),
+				new Tube(20, new Ray(Point3D.ZERO, new Vector(1, 10, 1))).setEmission(new Color(40, 40, 60))
+						.setMaterial(new Material().setKd(.5).setKs(.6).setShininess(100).setKr(.5).setGloss(7)));
 
-		scene.lights.add(new PointLight(new Color(200, 200, 200), new Point3D(0, 50, -60)));
+		scene.lights.add(new PointLight(new Color(200, 200, 200), new Point3D(0, 50, 350)));
 
 		Render render = new Render() //
 				.setImageWriter(new ImageWriter("glossiness", 1000, 1000)) //
